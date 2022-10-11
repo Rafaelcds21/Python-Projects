347f0e1a50ba0cb0c157e58a586b466bb1f6fa33
Rafaelcds21 <110067506+Rafaelcds21@users.noreply.github.com>
2022-10-11 12:41:27 -0300

    Update mega sena.py

--- mega sena.py
+++ mega sena.py
@@ -25,3 +25,21 @@ print(lista)
 
 grafico = px.histogram(lista)
 grafico.show()
+
+grafico_bola1 = px.histogram(bola1)
+grafico_bola1.show()
+
+grafico_bola2 = px.histogram(bola2)
+grafico_bola2.show()
+
+grafico_bola3 = px.histogram(bola3)
+grafico_bola3.show()
+
+grafico_bola4 = px.histogram(bola4)
+grafico_bola4.show()
+
+grafico_bola5 = px.histogram(bola5)
+grafico_bola5.show()
+
+grafico_bola6 = px.histogram(bola6)
+grafico_bola6.show()
