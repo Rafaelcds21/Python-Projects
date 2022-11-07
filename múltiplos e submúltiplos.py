dd123c115b67abcd9f3bb66abf511818801d79b9
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-11-07 15:51:45 -0300

    Create múltiplos e submúltiplos.py

---
+++ múltiplos e submúltiplos.py
@@ -0,0 +1,6 @@
+import numpy as np
+numero = int(input('Insira um número: '))
+dobro = 2*numero
+triplo = 3*numero
+raiz_quadrada = np.sqrt(numero)
+print(f'O dobro é {dobro}, o triplo é {triplo} e a raíz quadrada é {raiz_quadrada}.')
