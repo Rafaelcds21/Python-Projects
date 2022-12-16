9ab30d822996838d9619fcb515ec8d172d7f4321
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-16 10:57:06 -0300

    Create cálculo diferencial e integral.py

---
+++ matemática/cálculo diferencial e integral.py
@@ -0,0 +1,11 @@
+from sympy import *
+
+# derivada
+function = input("Insira a função aqui: ")
+derivada = diff(function)
+print(derivada)
+
+# integral
+function = input("Insira a função aqui: ")
+derivada = integrate(function)
+print(derivada)
