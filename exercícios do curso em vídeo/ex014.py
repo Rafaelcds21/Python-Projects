183d660387447f9834b2008596c41e05ae133a11
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 10:09:56 -0300

    Update ex014.py

--- exercícios do curso em vídeo/ex014.py
+++ exercícios do curso em vídeo/ex014.py
@@ -1,1 +1,3 @@
-
+temp_celsius = float(input('Insira a temperatura em graus Celsius: '))
+temp_fahrenheit = (9*temp_celsius/5)+32
+print(f'A temperatura de {temp_celsius} °C equivale a {temp_fahrenheit} °F')
