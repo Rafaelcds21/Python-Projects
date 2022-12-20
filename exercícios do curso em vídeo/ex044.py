5cc1b5fc4b77d642431e0e69e608391cfe3d3cad
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:11:03 -0300

    Update ex044.py

--- exercícios do curso em vídeo/ex044.py
+++ exercícios do curso em vídeo/ex044.py
@@ -1,1 +1,13 @@
-
+preco = float(input('Insira o valor do preço do produto: '))
+forma_pagamento = str(input('Insira a forma de pagamento: ')).lower()
+if 'à vista' or 'a vista' and 'dinheiro' or 'cheque' in forma_pagamento:
+    preco = preco-0.1*preco
+    print(preco)
+elif 'à vista' or 'a vista' and 'cartão' or 'cartao' in forma_pagamento:
+    preco = preco - 0.05 * preco
+    print(preco)
+elif '2x' and 'cartão' or 'cartao' in forma_pagamento:
+    print(preco)
+elif '3x' and 'cartão' or 'cartao ' in forma_pagamento:
+    preco = preco-0.2*preco
+    print(preco)
