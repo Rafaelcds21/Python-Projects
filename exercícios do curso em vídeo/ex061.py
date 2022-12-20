f46a29755a28042e898b6d5d0089877deebf6a69
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:57:48 -0300

    Update ex061.py

--- exercícios do curso em vídeo/ex061.py
+++ exercícios do curso em vídeo/ex061.py
@@ -1,1 +1,6 @@
-
+primeiro_termo = float(input('Insira o 1° termo da PA: '))
+razao = int(input('Insira a razão da PA: '))
+print(f'1° termo: {primeiro_termo}')
+for i in range(1, 10):
+    termo = primeiro_termo+i*razao
+    print(f'{i+1}° termo: {termo}')
