ce483f514d410991ff21a4ac1b66c077c90d1e0d
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:09:53 -0300

    Update ex042.py

--- exercícios do curso em vídeo/ex042.py
+++ exercícios do curso em vídeo/ex042.py
@@ -1,1 +1,19 @@
-
+a = float(input('Insira o comprimento de uma reta: '))
+b = float(input('Insira o comprimento de uma reta: '))
+c = float(input('Insira o comprimento de uma reta: '))
+if a < b + c:
+    if b < a + c:
+        if c < a + b:
+            print('As retas podem formar um triângulo.')
+            if a == b or a == c or b == c:
+                print('O triângulo é isósceles.')
+            elif a == b == c:
+                print('O triângulo é equilátero.')
+            elif a != b != c:
+                print('O triângulo é escaleno:')
+        else:
+            print('As retas não podem formar um triângulo.')
+    else:
+        print('As retas não podem formar um triângulo.')
+else:
+    print('As retas não podem formar um triângulo.')
