a832abb89191020b37ca18a6a9b8000b676a7777
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 10:09:25 -0300

    Update ex013.py

--- exercícios do curso em vídeo/ex013.py
+++ exercícios do curso em vídeo/ex013.py
@@ -1,1 +1,4 @@
-
+salario = float(input('Insira o salário do funcionário: '))
+aumento = 0.15
+salario_aumentado = salario + aumento * salario
+print(f'O salário de R${salario:.2f} aumentado a uma taxa de {100*aumento}% é igual a R${salario_aumentado:.2f}')
