44fb08c09aba25399ca0828d788af42ca48ecc24
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 10:59:23 -0300

    Update ex026.py

--- exercícios do curso em vídeo/ex026.py
+++ exercícios do curso em vídeo/ex026.py
@@ -1,1 +1,6 @@
-
+frase = str(input('Insira uma frase: ')).lower()
+contador = 0
+for i in range(0, len(frase)):
+    if frase[i] == 'a':
+        contador += 1
+print(f'A letra "A" aparece {contador} vezes.')
