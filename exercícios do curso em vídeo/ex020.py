6260f71f0ca34a302b6cfe7a9bae49b72a461c76
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 10:13:22 -0300

    Update ex020.py

--- exercícios do curso em vídeo/ex020.py
+++ exercícios do curso em vídeo/ex020.py
@@ -1,1 +1,12 @@
+from random import choice
+qtde_alunos = int(input('Insira a quantidade de alunos na sala: '))
+lista = []
+for i in range(1, qtde_alunos+1):
+    nome = str(input('Insira o nome do aluno: '))
+    lista.append(nome)
+qtde_apresentacoes = int(input('Insira o número de alunos que irão apresentar: '))
+for i in range(1, qtde_apresentacoes+1):
+    nome_sorteado = choice(lista)
+    print(f'O {i}° nome sorteado foi {nome_sorteado}.')
+    lista.remove(nome_sorteado)
 
