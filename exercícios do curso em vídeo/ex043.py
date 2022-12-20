611ccf2a6460f2746383a0533948415ddf399463
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:10:19 -0300

    Update ex043.py

--- exercícios do curso em vídeo/ex043.py
+++ exercícios do curso em vídeo/ex043.py
@@ -1,1 +1,19 @@
+peso = float(input('Insira o seu peso: '))
+altura = float(input('Insira a sua altura: '))
 
+imc = peso / (altura**2)
+
+if imc < 18.5:
+    print('Abaixo do peso.')
+
+elif 18.5 < imc < 25:
+    print('Peso ideal')
+
+elif 25 < imc < 30:
+    print('sobrepeso')
+
+elif 30 < imc < 40:
+    print('Obesidade')
+
+else:
+    print('Obesidade grave')
