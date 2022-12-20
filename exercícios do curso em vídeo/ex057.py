fdbac948766650f8ec81b645509b68b1798183f9
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:47:49 -0300

    Update ex057.py

--- exercícios do curso em vídeo/ex057.py
+++ exercícios do curso em vídeo/ex057.py
@@ -1,1 +1,8 @@
-
+sexo = str(input('Insira o seu sexo (M ou F): '))
+flag = True
+while flag:
+    if sexo == 'M' or sexo == 'F':
+        flag = False
+    else:
+        print('Refaça')
+        sexo = str(input('Insira o seu sexo (M ou F): '))
