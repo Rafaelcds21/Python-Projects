7dd9c866ab86768d9f3a6a7ca1e5799749ff0bfd
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:59:56 -0300

    Update ex065.py

--- exercícios do curso em vídeo/ex065.py
+++ exercícios do curso em vídeo/ex065.py
@@ -1,1 +1,29 @@
+soma = 0
+
+contador = 0
+
+maior = 0
+menor = 9999999
+
+flag = True
+
+while flag:
+    num = int(input("Digite um número: "))
+    soma += num
+    contador += 1
+
+    continuar = str(input("Quer continuar? ")).lower()
+
+    if continuar == 'sim':
+        if num > maior:
+            maior = num
+
+    else:
+        flag = False
+
+media = soma/contador
+
+print(f"Média: {media}")
+print(f"Maior número: {maior}")
+print(f"Menor número: {menor}")
 
