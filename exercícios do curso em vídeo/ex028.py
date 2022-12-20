34fa5deec17e52121322280fd6476c65dfe97eb1
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:00:30 -0300

    Update ex028.py

--- exercícios do curso em vídeo/ex028.py
+++ exercícios do curso em vídeo/ex028.py
@@ -1,1 +1,12 @@
-
+import random
+num_min = int(input('Insira o menor número da lista: '))
+num_max = int(input('Insira o maior numero da lista: '))
+lista = []
+for i in range(num_min, num_max):
+    lista.append(i)
+num_random = random.choice(lista)
+num_user = int(input(f'Insira um número inteiro entre {num_min} e {num_max}: '))
+if num_user == num_random:
+    print(f'Parabéns, você ganhou do computador! Você escolheu {num_user} e o computador escolheu {num_random}.')
+else:
+    print(f'Tente novamente, a máguina venceu. Você escolheu {num_user} e o computador escolheu {num_random}.')
