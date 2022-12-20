d076ce6ac7e2d099c4a6993ce55cfd3b19767b7d
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:02:54 -0300

    Update ex032.py

--- exercícios do curso em vídeo/ex032.py
+++ exercícios do curso em vídeo/ex032.py
@@ -1,1 +1,17 @@
+ano = int(input('Insira um ano: '))
+if ano % 4 == 0:
+    if ano % 100 == 0:
+        if ano % 400 == 0:
+            print(f'O ano {ano} é bissexto.')
+        else:
+            print(f'O ano {ano} não é bissexto')
+    else:
+        print(f'O ano {ano} é bissexto.')
+else:
+    print(f'O ano {ano} não é bissexto')
 
+# 1 - Se o ano for uniformemente divisível por 4, vá para a etapa 2. Caso contrário, vá para a etapa 5.
+# 2 - Se o ano for uniformemente divisível por 100, vá para a etapa 3. Caso contrário, vá para a etapa 4.
+# 3 - Se o ano for uniformemente divisível por 400, vá para a etapa 4. Caso contrário, vá para a etapa 5.
+# 4 - O ano é bissexto (tem 366 dias).
+# 5 - O ano não é um ano bissexto (tem 365 dias).
