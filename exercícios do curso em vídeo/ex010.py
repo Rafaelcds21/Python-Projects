4d489ccc79150b9a54a364990aad2123d729d215
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 10:07:42 -0300

    Update ex010.py

--- exercícios do curso em vídeo/ex010.py
+++ exercícios do curso em vídeo/ex010.py
@@ -1,1 +1,4 @@
-
+valor_reais = float(input('Insira a quantidade de dinheiro em reais: '))
+cotacao_dolar = 5.0
+valor_convertido = valor_reais/cotacao_dolar
+print(f'O valor R${valor_reais:.2f} equivale a ${valor_convertido:.2f}')
