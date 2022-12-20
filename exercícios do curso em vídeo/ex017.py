bc89e090fab40928d6d1337ba6f8bab1c8e5ab47
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 10:11:31 -0300

    Update ex017.py

--- exercícios do curso em vídeo/ex017.py
+++ exercícios do curso em vídeo/ex017.py
@@ -1,1 +1,6 @@
+import numpy as np
+cateto_oposto = float(input('Insira o valor do cateto oposto: '))
+cateto_adjacente = float(input('Insira o valor do cateto adjacente: '))
+hipotenusa = np.sqrt(cateto_oposto**2+cateto_adjacente**2)
+print(f'O valor da hipotenusa é {hipotenusa}')
 
