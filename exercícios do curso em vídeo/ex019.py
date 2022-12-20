d2d1792fbf53b2e785ec950bc93246e4df39f24a
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 10:12:53 -0300

    Update ex019.py

--- exercícios do curso em vídeo/ex019.py
+++ exercícios do curso em vídeo/ex019.py
@@ -1,1 +1,9 @@
+from random import choice
+qtde_alunos = int(input('Insira a quantidade de alunos na sala: '))
+lista = []
+for i in range(1, qtde_alunos+1):
+    nome = str(input('Insira o nome do aluno: '))
+    lista.append(nome)
+nome_sorteado = choice(lista)
+print(f'O nome sorteado foi {nome_sorteado}.')
 
