a4664a88cb1659c958cc30859d4b1a2dbdac7595
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:49:38 -0300

    Update ex060.py

--- exercícios do curso em vídeo/ex060.py
+++ exercícios do curso em vídeo/ex060.py
@@ -1,1 +1,10 @@
+def fatorial(x):
+    resp_fatorial = 1
+    for i in range(1, x+1):
+        resp_fatorial *= i
+    return resp_fatorial
 
+
+num = int(input("Insira um número: "))
+fatorial_num = fatorial(num)
+print(fatorial_num)
