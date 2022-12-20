0e0b4aaf0929aa528be788e8771cb3a192b0c762
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:05:58 -0300

    Update ex036.py

--- exercícios do curso em vídeo/ex036.py
+++ exercícios do curso em vídeo/ex036.py
@@ -1,1 +1,9 @@
-
+valor_casa = float(input('Insira o valor da casa: '))
+salario = float(input('Insira o valor do salário: '))
+anos = int(input('Insira por quantos anos a dívida será paga:'))
+meses = 12 * anos
+prestacao = valor_casa / meses
+if prestacao <= 0.3*salario:
+    print('Empréstimo aprovado!')
+else:
+    print('Empréstimo negado!')
