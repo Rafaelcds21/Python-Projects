8cd947a28a46cc4d038c7495da66afbe6126cc80
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:24:08 -0300

    Update ex052.py

--- exercícios do curso em vídeo/ex052.py
+++ exercícios do curso em vídeo/ex052.py
@@ -1,1 +1,13 @@
+numero = int(input('Insira um número: '))
+
+contador = 0
+
+for i in range(2, numero+1):
+    if numero % i == 0:
+        contador += 1
+
+if contador > 1:
+    print('O número não é primo.')
+else:
+    print('O número é primo')
 
