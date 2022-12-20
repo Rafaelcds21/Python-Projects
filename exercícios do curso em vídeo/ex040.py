52788f26d7bd4c001d91ecb2975271fdaa032f54
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:08:17 -0300

    Update ex040.py

--- exercícios do curso em vídeo/ex040.py
+++ exercícios do curso em vídeo/ex040.py
@@ -1,1 +1,10 @@
-
+n1 = float(input('Insira a 1° nota do aluno: '))
+n2 = float(input('Insira a 2° nota do aluno: '))
+media = (n1+n2)/2
+if media >= 7.0:
+    print('APROVADO')
+else:
+    if 5.0 <= media <= 6.9:
+        print('RECUPERAÇÃO')
+    else:
+        print('REPROVADO')
