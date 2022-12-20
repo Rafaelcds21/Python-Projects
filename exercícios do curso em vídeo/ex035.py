fc684cc6f62487adf3c369b9c6019ecc72081932
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:05:06 -0300

    Update ex035.py

--- exercícios do curso em vídeo/ex035.py
+++ exercícios do curso em vídeo/ex035.py
@@ -1,1 +1,13 @@
-
+a = float(input('Insira o comprimento de uma reta: '))
+b = float(input('Insira o comprimento de uma reta: '))
+c = float(input('Insira o comprimento de uma reta: '))
+if a < b + c:
+    if b < a + c:
+        if c < a + b:
+            print('As retas podem formar um triângulo.')
+        else:
+            print('As retas não podem formar um triângulo.')
+    else:
+        print('As retas não podem formar um triângulo.')
+else:
+    print('As retas não podem formar um triângulo.')
