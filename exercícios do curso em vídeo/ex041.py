343a507898b1bdf02fe824ee013263cad519eaae
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:08:47 -0300

    Update ex041.py

--- exercícios do curso em vídeo/ex041.py
+++ exercícios do curso em vídeo/ex041.py
@@ -1,1 +1,26 @@
+from datetime import datetime
 
+data = datetime.now()
+ano_atual = data.year
+
+ano_nascimento = int(input('Insira o ano de nascimento: '))
+
+idade = ano_atual-ano_nascimento
+
+if idade <= 9:
+    print('MIRIM')
+
+else:
+    if 9 < idade <= 14:
+        print('INFANTIL')
+
+    else:
+        if 14 < idade <= 19:
+            print('JÚNIOR')
+
+        else:
+            if 19 < idade <= 25:
+                print('SÊNIOR')
+
+            else:
+                print('IDOSO')
