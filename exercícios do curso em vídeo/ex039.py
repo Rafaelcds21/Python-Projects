3fce3855f2b6c11b4eb4beedb705e3f0e36e1828
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:07:44 -0300

    Update ex039.py

--- exercícios do curso em vídeo/ex039.py
+++ exercícios do curso em vídeo/ex039.py
@@ -1,1 +1,11 @@
-
+from datetime import datetime
+ano = int(input('Insira o ano de nascimento: '))
+ano_prov_alis = datetime.now().year
+diferenca = ano_prov_alis-ano
+if diferenca < 18:
+    print('Ainda é cedo.')
+else:
+    if diferenca == 18:
+        print('Ano de alistamento.')
+    else:
+        print('Já passou do tempo de se alistar.')
