f1c146bf99b78898d0f9f826b607df44cfe6964c
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 10:10:25 -0300

    Update ex015.py

--- exercícios do curso em vídeo/ex015.py
+++ exercícios do curso em vídeo/ex015.py
@@ -1,1 +1,5 @@
-
+valor_km = float(input('Insira o valor dos quilômetros rodados: '))
+qtde_dias = int(input('Insira a quantidade de dias que o carro foi usado: '))
+valor = (60*qtde_dias)+(0.15*valor_km)
+print(f'Dado que o carro rodou um total de {valor_km} quilômetros e foi usado por {qtde_dias} dias, o valor total a '
+      f'ser pago é R${valor:.2f}.')
