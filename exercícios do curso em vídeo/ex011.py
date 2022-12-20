ab4fd05676e9730fe3fb29289f2df2013d4f87e2
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 10:08:27 -0300

    Update ex011.py

--- exercícios do curso em vídeo/ex011.py
+++ exercícios do curso em vídeo/ex011.py
@@ -1,1 +1,6 @@
-
+largura = float(input('Insira o valor da largura da parede: '))
+altura = float(input('Insira o valor a altura da parede: '))
+area = largura * altura
+qtde_tinta = area/2
+print(f'Dada a parede com uma largura de {largura} metros e uma altura de {altura} metros, a área é de {area} metros '
+      f'quadrado e serão necessários {qtde_tinta} litros de tinta')
