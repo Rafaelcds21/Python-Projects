27919b9f003e05edbaa331d2f8d4905004b06563
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:11:35 -0300

    Update ex045.py

--- exercícios do curso em vídeo/ex045.py
+++ exercícios do curso em vídeo/ex045.py
@@ -1,1 +1,45 @@
+import random
+
+
+def mainloop():
+    if player_input == 'pedra':
+        if maquina_input == 'pedra':
+            print(f'Empate. A sua escolha foi {player_input} e a escolha da máquina foi {maquina_input}')
+        elif maquina_input == 'papel':
+            print(f'Você perdeu. A sua escolha foi {player_input} e a escolha da máquina foi {maquina_input}')
+        else:
+            print(f'Você ganhou. A sua escolha foi {player_input} e a escolha da máquina foi {maquina_input}')
+
+    elif player_input == 'papel':
+        if maquina_input == 'pedra':
+            print(f'Você ganhou. A sua escolha foi {player_input} e a escolha da máquina foi {maquina_input}')
+        elif maquina_input == 'papel':
+            print(f'Empate. A sua escolha foi {player_input} e a escolha da máquina foi {maquina_input}')
+        else:
+            print(f'Você perdeu. A sua escolha foi {player_input} e a escolha da máquina foi {maquina_input}')
+
+    else:
+        if maquina_input == 'pedra':
+            print(f'Você perdeu. A sua escolha foi {player_input} e a escolha da máquina foi {maquina_input}')
+        elif maquina_input == 'papel':
+            print(f'Você ganhou. A sua escolha foi {player_input} e a escolha da máquina foi {maquina_input}')
+        else:
+            print(f'Empate. A sua escolha foi {player_input} e a escolha da máquina foi {maquina_input}')
+
+
+iniciar = input('Deseja iniciar? (sim/não) ').lower()
+
+conjunto = ['pedra', 'papel', 'tesoura']
+
+while True:
+    if iniciar == 'sim':
+        player_input = str(input('Insira a sua escolha: '))
+        maquina_input = random.choice(conjunto)
+        mainloop()
+
+    else:
+        break
+
+    continuar = input('Você deseja continuar? (sim/não) ')
+    iniciar = continuar
 
