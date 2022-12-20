119af663b0b4c97134cebe8369ed5c9c26996058
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:07:12 -0300

    Update ex038.py

--- exercícios do curso em vídeo/ex038.py
+++ exercícios do curso em vídeo/ex038.py
@@ -1,1 +1,9 @@
-
+num1 = int(input('Insira um valor inteiro: '))
+num2 = int(input('Insira um valor inteiro: '))
+if num1 > num2:
+    print(f'{num1} é maior que {num2}.')
+else:
+    if num2>num1:
+        print(f'{num2} é maior que {num1}.')
+    else:
+        print(f'{num1} e {num2} são iguais.')
