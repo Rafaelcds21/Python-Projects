678e95b7f74251decbec4805bc5a14978c7e8f97
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:59:28 -0300

    Update ex066.py

--- exercícios do curso em vídeo/ex066.py
+++ exercícios do curso em vídeo/ex066.py
@@ -1,1 +1,13 @@
-
+flag = True
+contador = 0
+soma = 0
+while flag:
+    num = int(input("Insira um input: "))
+    if num == 99:
+        flag = False
+        soma -= 99
+        contador -= 1
+    soma += num
+    contador += 1
+print(f"Soma: {soma}")
+print(f"Quantidade de números: {contador}")
