bcb85b54f673d79fa43ad65bfd8afccfb70bfc3f
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 10:08:57 -0300

    Update ex012.py

--- exercícios do curso em vídeo/ex012.py
+++ exercícios do curso em vídeo/ex012.py
@@ -1,1 +1,4 @@
-
+preco = float(input('Insira o valor do preço do produto: '))
+desconto = 0.05
+preco_ajustado = preco - desconto * preco
+print(f'O valor de R${preco:.2f} com um desconto a uma taxa de {100*desconto}% é igual a R${preco_ajustado:.2f}')
