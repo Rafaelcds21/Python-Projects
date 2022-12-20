e74d5cad6bad7cc027390e93588373c320d13c8b
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:06:39 -0300

    Update ex037.py

--- exercícios do curso em vídeo/ex037.py
+++ exercícios do curso em vídeo/ex037.py
@@ -1,1 +1,16 @@
-
+numero = int(input('Insira um número inteiro: '))
+print('-'*15)
+print('Digite:')
+print('1 para binário')
+print('2 para octal')
+print('3 para hexadecimal')
+print('-'*15)
+base_nova = int(input('Insira a base nova: '))
+if base_nova == 1:
+    numero = bin(numero)
+else:
+    if base_nova == 2:
+        numero = oct(numero)
+    else:
+        numero = hex(numero)
+print(numero)
