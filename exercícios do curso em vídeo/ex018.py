f15b3538194ae131120189fdc09c9de57b9d6d08
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 10:12:03 -0300

    Update ex018.py

--- exercícios do curso em vídeo/ex018.py
+++ exercícios do curso em vídeo/ex018.py
@@ -1,1 +1,6 @@
-
+import numpy as np
+angulo = float(input('Insira o valor do ângulo: '))
+seno = np.sin(angulo)
+cosseno = np.cos(angulo)
+tangente = np.tan(angulo)
+print(f'O seno é {seno}, o cosseno é {cosseno} e a tangente é {tangente}')
