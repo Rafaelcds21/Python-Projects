7fed64297c7629fca007f06df2d2976082cc4e80
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 10:06:41 -0300

    Update ex008.py

--- exercícios do curso em vídeo/ex008.py
+++ exercícios do curso em vídeo/ex008.py
@@ -1,1 +1,4 @@
-
+valor_metro = int(input('Insira um valor em metros: '))
+valor_centimetro = valor_metro*100
+valor_milimetro = valor_metro*1000
+print(f'O valor {valor_metro} metros é equivalente á {valor_centimetro} centímetros e {valor_milimetro} milímetros.')
