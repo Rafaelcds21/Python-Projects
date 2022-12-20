c8cca19366222892cf2c4540e3acd4632ed837b2
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 10:13:57 -0300

    Update ex022.py

--- exercícios do curso em vídeo/ex022.py
+++ exercícios do curso em vídeo/ex022.py
@@ -1,1 +1,7 @@
-
+nome = str(input('Insira o seu nome: '))
+nome_maiusculo = nome.upper()
+nome_minusculo = nome.lower()
+nome_junto = nome.replace(' ', '')
+print(nome_junto)
+print(f'O nome em letras maiúsculas é {nome_maiusculo}, o nome em letras minúsculas é {nome_minusculo} e o nome tem '
+      f'{len(nome_junto)} letras.')
