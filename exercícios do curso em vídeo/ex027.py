5f630d449e5639c064213f3b885f01ce81c0c459
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 10:59:59 -0300

    Update ex027.py

--- exercícios do curso em vídeo/ex027.py
+++ exercícios do curso em vídeo/ex027.py
@@ -1,1 +1,7 @@
-
+nome = str(input('Insira um nome: '))
+nomes_separados = nome.split(' ')
+contador = -1
+for i in range(0, len(nomes_separados)):
+    contador += 1
+print(contador)
+print(f'O 1° nome é {nomes_separados[0]} e o último nome é {nomes_separados[contador]}')
