3e13e655adc7c820d2840e88f2c8a119ffba3635
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:46:33 -0300

    Update ex055.py

--- exercícios do curso em vídeo/ex055.py
+++ exercícios do curso em vídeo/ex055.py
@@ -1,1 +1,10 @@
-
+maior = 0
+menor = 99999999
+for i in range(1, 7):
+    peso = float(input('Insira um peso: '))
+    if peso > maior:
+        maior = peso
+    if peso < menor:
+        menor = peso
+print(maior)
+print(menor)
