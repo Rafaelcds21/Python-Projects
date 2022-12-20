a730e013b7da0407028679eb31b5845d4dd37a03
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:58:43 -0300

    Update ex063.py

--- exercícios do curso em vídeo/ex063.py
+++ exercícios do curso em vídeo/ex063.py
@@ -1,1 +1,10 @@
+seq_fib = "0 - 1 - "+f"{1} - "
+print(seq_fib)
+
+soma = 0
+qtde_elementos = int(input("Insira um número inteiro: "))
+for i in range(1, qtde_elementos+1):
+    soma += i
+    seq_fib = "0 - 1 - " + f"{soma} - "
+    print(seq_fib)
 
