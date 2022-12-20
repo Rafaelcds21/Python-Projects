4d0fa0dd865e58192dc8e4f0c976d8c1f0f54291
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:02:03 -0300

    Update ex031.py

--- exercícios do curso em vídeo/ex031.py
+++ exercícios do curso em vídeo/ex031.py
@@ -1,1 +1,7 @@
-
+valor_km = float(input('Insira o valor da distância da viagem: '))
+if valor_km <= 200.0:
+    valor_viagem = 0.50 * valor_km
+    print(f'O valor da viagem é R${valor_viagem:.2f}.')
+else:
+    valor_viagem = 0.45 * valor_km
+    print(f'O valor da viagem é R${valor_viagem:.2f}.')
