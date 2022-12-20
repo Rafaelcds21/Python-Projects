95d0face8102465df12e4f925864bd985734d06a
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 12:26:43 -0300

    Update ex073.py

--- exercícios do curso em vídeo/ex073.py
+++ exercícios do curso em vídeo/ex073.py
@@ -1,1 +1,15 @@
+tupla_time = ('flamengo', 'vasco', 'botafogo', 'fluminense', 'chapecoense', 'piracicaba', 'a', 'b', 'c', 'd', 'e', 'f',
+              'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n')
 
+# a
+for i in range(0, 5):
+    print(f'{i+1}°: {tupla_time[i]}')
+
+# b
+for i in range(16, 20):
+    print(f'{i}°: {tupla_time[i]}')
+
+# c
+
+# d
+print(f"{tupla_time.index('chapecoense')+1}° posição")
