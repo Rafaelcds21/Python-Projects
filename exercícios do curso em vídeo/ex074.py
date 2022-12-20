da8dafa81543e900a1c9961e04daac6b91d17d7b
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 12:27:29 -0300

    Update ex074.py

--- exercícios do curso em vídeo/ex074.py
+++ exercícios do curso em vídeo/ex074.py
@@ -1,1 +1,25 @@
+import random
 
+maior = 0
+menor = 99999999
+
+num1 = random.randrange(0, 9)
+num2 = random.randrange(0, 9)
+num3 = random.randrange(0, 9)
+num4 = random.randrange(0, 9)
+num5 = random.randrange(0, 9)
+
+tupla_numeros = (num1, num2, num3, num4, num5)
+
+print(tupla_numeros)
+
+for i in range(0, 5):
+    print(tupla_numeros[i])
+
+    if tupla_numeros[i] > maior:
+        maior = tupla_numeros[i]
+
+    if tupla_numeros[i] < menor:
+        menor = tupla_numeros[i]
+
+print(f"maior: {maior} ,menor: {menor}")
