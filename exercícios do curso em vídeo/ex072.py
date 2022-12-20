04fac2982172c87365c58e7de0e6de776e37188a
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 12:26:17 -0300

    Update ex072.py

--- exercícios do curso em vídeo/ex072.py
+++ exercícios do curso em vídeo/ex072.py
@@ -1,1 +1,4 @@
-
+numero_extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze'
+                  , 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+numero = int(input("Insira um número: "))
+print(numero_extenso[numero])
