796986a5eed569b35eb00f509b519ecc7119b06b
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:03:55 -0300

    Update ex033.py

--- exercícios do curso em vídeo/ex033.py
+++ exercícios do curso em vídeo/ex033.py
@@ -1,1 +1,7 @@
-
+lista = []
+for i in range(0, 3):
+    numero = int(input('Insira um número: '))
+    lista.append(numero)
+lista = sorted(lista)
+print(lista)
+print(f'O maior é {lista[2]} e o menor é {lista[0]}')
