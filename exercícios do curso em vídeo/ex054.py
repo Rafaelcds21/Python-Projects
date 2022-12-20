c9049e244f9bec4e90d022732b7065576b568c6d
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:25:27 -0300

    Update ex054.py

--- exercícios do curso em vídeo/ex054.py
+++ exercícios do curso em vídeo/ex054.py
@@ -1,1 +1,9 @@
-
+from datetime import datetime
+ano_atual = datetime.now().year
+for i in range(1, 8):
+    ano_usuario = int(input('Insira o ano de nascimento: '))
+    diferenca = ano_atual - ano_usuario
+    if diferenca>18:
+        print('Maioridade')
+    else:
+        print('Minoridade')
