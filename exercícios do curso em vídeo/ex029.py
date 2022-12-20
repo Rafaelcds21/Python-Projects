8d3db4f7a5ff460a7cea58d4b88e616e0adb812f
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-12-20 11:01:06 -0300

    Update ex029.py

--- exercícios do curso em vídeo/ex029.py
+++ exercícios do curso em vídeo/ex029.py
@@ -1,1 +1,10 @@
-
+velocidade = str(input('Insira a velocidade do carro: '))
+if velocidade[2] == ',':
+    velocidade = velocidade.replace(',', '.')
+velocidade = float(velocidade)
+if velocidade > 80:
+    diferenca = velocidade-80
+    multa = 7.00*diferenca
+    print(f'A multa foi de R${multa:.2f}')
+else:
+    print('O carro está dentro do limite.')
